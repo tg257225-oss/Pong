3ecdca7cf3744785be792352ce743ab1f5e924cd
Mapplect <tg257225@gmail.com>
2026-07-23 12:03:51 -0400

    ball

--- pong.py
+++ pong.py
@@ -11,7 +11,7 @@ window.tracer(0)
 pad_1 = turtle.Turtle()
 pad_1.speed(0)
 pad_1.shape("square")
-pad_1.shapesize(stretch_wid=5, stretch_len=1)
+pad_1.shapesize(stretch_wid=5.5, stretch_len=1)
 pad_1.color("white")
 pad_1.penup()
 pad_1.goto(-400, 0)
@@ -19,13 +19,18 @@ pad_1.goto(-400, 0)
 pad_2 = turtle.Turtle()
 pad_2.speed(0)
 pad_2.shape("square")
-pad_2.shapesize(stretch_wid=5, stretch_len=1)
+pad_2.shapesize(stretch_wid=5.5, stretch_len=1)
 pad_2.color("white")
 pad_2.penup()
 pad_2.goto(400, 0)
-
-
 # ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.shapesize(stretch_wid=1.5, stretch_len=1.5)
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 
 while True:
