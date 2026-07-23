d74ba89eb098efc66f4a8fa889b6f957bec55b1d
Mapplect <tg257225@gmail.com>
2026-07-23 11:59:14 -0400

    Made both paddles

--- pong.py
+++ pong.py
@@ -6,6 +6,7 @@ window.bgcolor("#677087")
 window.setup(width=900, height=600)
 window.tracer(0)
 
+
 # paddle numero 1
 pad_1 = turtle.Turtle()
 pad_1.speed(0)
@@ -14,16 +15,14 @@ pad_1.shapesize(stretch_wid=5, stretch_len=1)
 pad_1.color("white")
 pad_1.penup()
 pad_1.goto(-400, 0)
-
-
-turtle.up()
-turtle.goto(-200,-150)
-turtle.color("blue")
-turtle.down()
-turtle.dot()
-turtle.fd(400)
-turtle.dot()
 # paddle numero 2
+pad_2 = turtle.Turtle()
+pad_2.speed(0)
+pad_2.shape("square")
+pad_2.shapesize(stretch_wid=5, stretch_len=1)
+pad_2.color("white")
+pad_2.penup()
+pad_2.goto(400, 0)
 
 
 # ball
