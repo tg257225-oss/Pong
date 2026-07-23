4e80ede01719ed0534cbef11288ae2c57ac7be99
Mapplect <tg257225@gmail.com>
2026-07-23 12:45:19 -0400

    second paddle moves using up and down arrow keys

--- pong.py
+++ pong.py
@@ -15,6 +15,7 @@ pad_1.shapesize(stretch_wid=5.5, stretch_len=1)
 pad_1.color("white")
 pad_1.penup()
 pad_1.goto(-400, 0)
+
 # paddle numero 2
 pad_2 = turtle.Turtle()
 pad_2.speed(0)
@@ -23,6 +24,7 @@ pad_2.shapesize(stretch_wid=5.5, stretch_len=1)
 pad_2.color("white")
 pad_2.penup()
 pad_2.goto(400, 0)
+
 # ball
 ball = turtle.Turtle()
 ball.speed(0)
@@ -31,6 +33,40 @@ ball.shapesize(stretch_wid=1.5, stretch_len=1.5)
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+
+# move da paddles
+def pad_1_up():
+    y = pad_1.ycor()
+    y+=15
+    pad_1.sety(y)
+
+def pad_1_down():
+    y = pad_1.ycor()
+    y-=15
+    pad_1.sety(y)
+
+def pad_2_up():
+    y = pad_2.ycor()
+    y += 15
+    pad_2.sety(y)
+
+def pad_2_down():
+    y = pad_2.ycor()
+    y-=15
+    pad_2.sety(y)
+
+window.listen()
+window.onkeypress(pad_1_up, "w")
+window.onkeypress(pad_1_down, "s")
+window.onkeypress(pad_2_up, "Up")
+window.onkeypress(pad_2_down, "Down")
+
+
+
+
+
+
+
 
 
 while True:
