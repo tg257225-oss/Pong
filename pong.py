f0d165967e624b862896962ca8d0e3f5232fac6b
Mapplect <tg257225@gmail.com>
2026-07-24 12:19:24 -0400

    Menu mainly done and 3rd variation, going to add an AI option so you can play by yourself

--- pong.py
+++ pong.py
@@ -63,34 +63,46 @@ pen.hideturtle()
 
 pen1 = turtle.Turtle()
 pen1.speed(0)
-pen1.color("black")
+pen1.color("#b0afab")
 pen1.penup()
 pen1.hideturtle()
 
+pen2 = turtle.Turtle()
+pen2.speed(0)
+pen2.color("#46579e")
+pen2.penup()
+pen2.hideturtle()
 
-# box
+pen3 = turtle.Turtle()
+pen3.speed(0)
+pen3.color("#E87DA3")
+pen3.penup()
+pen3.hideturtle()
+
+
+# btn
 btn1 = turtle.Turtle()
 btn1.shape("square")
 btn1.shapesize(stretch_wid=3, stretch_len=15)
 btn1.penup()
 btn1.speed(0)
-btn1.color("white")
+btn1.color("#b0afab")
 
 btn2 = turtle.Turtle()
 btn2.shape("square")
 btn2.shapesize(stretch_wid=3, stretch_len=15)
 btn2.penup()
 btn2.speed(0)
-btn2.color("blue")
+btn2.color("#46579e")
 
 btn3 = turtle.Turtle()
 btn3.shape("square")
 btn3.shapesize(stretch_wid=3, stretch_len=15)
 btn3.penup()
 btn3.speed(0)
-btn3.color("green")
+btn3.color("#E87DA3")
 
-
+ai_speed = 4
 
 # dict
 keys_pressed = {
@@ -127,6 +139,10 @@ def handle_click1(x, y):
 def handle_click2(x, y):
     game_2_press()
 
+def handle_click3(x, y):
+    game_3_press()
+
+
 game_state = "game1"
 
 def draw_menu():
@@ -145,23 +161,29 @@ def draw_menu():
     pen1.clear()
     btn1.clearstamps()
 
-    btn1.goto(0, 180)
-    btn2.goto(0, 100)
-    btn3.goto(0, 20)
+    btn1.goto(0, 120)
+    btn2.goto(0, 40)
+    btn3.goto(0, -40)
 
 
     pen.goto(0,230)
     pen.write("PONG", align="center", font=("Lucida Sans", 30, "bold"))
 
 
-    #pen1.goto(-200, 180)
-    #pen1.write("Normal", align="center", font=("Lucida Sans", 20, "bold"))
+    pen1.goto(-300, 105)
+    pen1.write("Normal", align="center", font=("Lucida Sans", 20, "bold"))
+    pen2.goto(-300, 25)
+    pen2.write("Speed", align="center", font=("Lucida Sans", 20, "bold"))
+    pen3.goto(-300, -55)
+    pen3.write("Large Paddles", align="center", font=("Lucida Sans", 20, "bold"))
+
 
 
     window.listen()
     window.onkeypress(game_1_press, "1")
     btn1.onclick(handle_click1)
     btn2.onclick(handle_click2)
+    btn3.onclick(handle_click3)
 
 def game_1_press():
     start_game_1()
@@ -169,12 +191,17 @@ def game_1_press():
 def game_2_press():
     start_game_2()
 
+def game_3_press():
+    start_game_3()
+
 def start_game_1():
     global game_state
     if game_state == "menu":
         game_state = "game1"
         pen.clear()
         pen1.clear()
+        pen2.clear()
+        pen3.clear()
         btn1.hideturtle()
         btn2.hideturtle()
         btn3.hideturtle()
@@ -210,6 +237,46 @@ def start_game_2():
         game_state = "game2"
         pen.clear()
         pen1.clear()
+        pen2.clear()
+        pen3.clear()
+        btn1.hideturtle()
+        btn2.hideturtle()
+        btn3.hideturtle()
+        pad_1.showturtle()
+        pad_2.showturtle()
+        ball.showturtle()
+        pen.goto(0, 230)
+        pen.write("PLAYER 1: 0                                PLAYER 2: 0", align="center",
+                  font=("Lucida Sans", 24, "bold"))
+        pen.goto(0, 230)
+        pen.write("Click 'E' to go back to the main menu.", align="center",
+                  font=("Lucida Sans", 10, "normal"))
+
+
+        window.listen()
+        window.onkeypress(draw_menu, "e")
+
+        window.onkeypress(press_w, "w")
+        window.onkeyrelease(release_w, "w")
+
+        window.onkeypress(press_s, "s")
+        window.onkeyrelease(release_s, "s")
+
+        window.onkeypress(press_up, "Up")
+        window.onkeyrelease(release_up, "Up")
+
+        window.onkeypress(press_down, "Down")
+        window.onkeyrelease(release_down, "Down")
+
+
+def start_game_3():
+    global game_state
+    if game_state == "menu":
+        game_state = "game3"
+        pen.clear()
+        pen1.clear()
+        pen2.clear()
+        pen3.clear()
         btn1.hideturtle()
         btn2.hideturtle()
         btn3.hideturtle()
@@ -407,5 +474,76 @@ while True:
                 ball.ycor() < pad_1.ycor() + 95 and ball.ycor() > pad_1.ycor() - 95):
             ball.setx(-390)
             ball.dx *= -1.2
+            sound.play()
+
+    if game_state == "game3":
+
+        window.bgcolor("#E87DA3")
+        pad_1.shapesize(stretch_wid=18.5, stretch_len=1)
+        pad_2.shapesize(stretch_wid=18.5, stretch_len=1)
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+
+
+
+        # check da border
+        if ball.ycor() > 282:
+            ball.sety(282)
+            ball.dy *= -1
+            sound.play()
+
+        if ball.ycor() < -282:
+            ball.sety(-282)
+            ball.dy *= -1
+            sound.play()
+
+        if ball.xcor() > 435:
+            ball.goto(0, 0)
+            ball.dx *= -1
+            scr_1 += 1
+            pen.clear()
+            ding.play()
+            pen.write("PLAYER 1: {}                                PLAYER 2: {}".format(scr_1, scr_2), align="center",
+                      font=("Lucida Sans", 24, "bold"))
+            pen.write("Click 'E' to go back to the main menu.", align="center",
+                      font=("Lucida Sans", 10, "normal"))
+
+        if ball.xcor() < -435:
+            ball.goto(0, 0)
+            ball.dx *= -1
+            scr_2 += 1
+            pen.clear()
+            ding.play()
+            pen.write("PLAYER 1: {}                                PLAYER 2: {}".format(scr_1, scr_2), align="center",
+                      font=("Lucida Sans", 24, "bold"))
+            pen.write("Click 'E' to go back to the main menu.", align="center",
+                      font=("Lucida Sans", 10, "normal"))
+
+        if not pad_1.ycor() < 124:
+            pad_1.sety(124)
+
+
+        if not pad_1.ycor() > -124:
+            pad_1.sety(-124)
+
+
+        if not pad_2.ycor() < 120:
+            pad_2.sety(124)
+
+
+        if not pad_2.ycor() > -124:
+            pad_2.sety(-124)
+
+
+        # paddle ball collisions
+        if (ball.xcor() > 390 and ball.xcor() < 400) and (ball.ycor() < pad_2.ycor()+ 200 and ball.ycor() > pad_2.ycor() - 200):
+            ball.setx(390)
+            ball.dx *=-1
+            sound.play()
+
+        if (ball.xcor() < -390 and ball.xcor() > -400) and (ball.ycor() < pad_1.ycor()+ 200 and ball.ycor() > pad_1.ycor() - 200):
+            ball.setx(-390)
+            ball.dx *=-1
             sound.play()
 
