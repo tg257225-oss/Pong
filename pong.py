fa137b42e2add93820892bfc326fa69b7492afa2
Mapplect <tg257225@gmail.com>
2026-07-23 20:04:21 -0400

    New variation!!

--- pong.py
+++ pong.py
@@ -61,6 +61,34 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 
+pen1 = turtle.Turtle()
+pen1.speed(0)
+pen1.color("black")
+pen1.penup()
+pen1.hideturtle()
+
+
+# box
+btn1 = turtle.Turtle()
+btn1.shape("square")
+btn1.shapesize(stretch_wid=3, stretch_len=15)
+btn1.penup()
+btn1.speed(0)
+btn1.color("white")
+
+btn2 = turtle.Turtle()
+btn2.shape("square")
+btn2.shapesize(stretch_wid=3, stretch_len=15)
+btn2.penup()
+btn2.speed(0)
+btn2.color("blue")
+
+btn3 = turtle.Turtle()
+btn3.shape("square")
+btn3.shapesize(stretch_wid=3, stretch_len=15)
+btn3.penup()
+btn3.speed(0)
+btn3.color("green")
 
 
 
@@ -93,7 +121,11 @@ def release_down():
     keys_pressed["Down"] = False
 
 
+def handle_click1(x, y):
+    game_1_press()
 
+def handle_click2(x, y):
+    game_2_press()
 
 game_state = "game1"
 
@@ -103,30 +135,62 @@ def draw_menu():
     pad_1.hideturtle()
     pad_2.hideturtle()
     ball.hideturtle()
+
+    btn1.showturtle()
+    btn2.showturtle()
+    btn3.showturtle()
+
     pen.clear()
-    pen.goto(0,250)
+    pen1.clear()
+    btn1.clearstamps()
+
+    btn1.goto(0, 180)
+    btn2.goto(0, 100)
+    btn3.goto(0, 20)
+
+
+    pen.goto(0,230)
     pen.write("PONG", align="center", font=("Lucida Sans", 30, "bold"))
-    pen.goto(0, 150)
-    pen.write("Press '1' to choose normal", align="center", font=("Lucida Sans", 20, "bold"))
+
+
+    #pen1.goto(-200, 180)
+    #pen1.write("Normal", align="center", font=("Lucida Sans", 20, "bold"))
+
+
     window.listen()
     window.onkeypress(game_1_press, "1")
+    btn1.onclick(handle_click1)
+    btn2.onclick(handle_click2)
 
 def game_1_press():
     start_game_1()
+
+def game_2_press():
+    start_game_2()
 
 def start_game_1():
     global game_state
     if game_state == "menu":
         game_state = "game1"
         pen.clear()
-
+        pen1.clear()
+        btn1.hideturtle()
+        btn2.hideturtle()
+        btn3.hideturtle()
         pad_1.showturtle()
         pad_2.showturtle()
         ball.showturtle()
-        pen.goto(0, 260)
+        pen.goto(0, 230)
         pen.write("PLAYER 1: 0                                PLAYER 2: 0", align="center",
                   font=("Lucida Sans", 24, "bold"))
+        pen.goto(0, 230)
+        pen.write("Click 'E' to go back to the main menu.", align="center",
+                  font=("Lucida Sans", 10, "normal"))
+
+
         window.listen()
+        window.onkeypress(draw_menu, "e")
+
         window.onkeypress(press_w, "w")
         window.onkeyrelease(release_w, "w")
 
@@ -139,7 +203,40 @@ def start_game_1():
         window.onkeypress(press_down, "Down")
         window.onkeyrelease(release_down, "Down")
 
+def start_game_2():
+    global game_state
+    if game_state == "menu":
+        game_state = "game2"
+        pen.clear()
+        pen1.clear()
+        btn1.hideturtle()
+        btn2.hideturtle()
+        btn3.hideturtle()
+        pad_1.showturtle()
+        pad_2.showturtle()
+        ball.showturtle()
+        pen.goto(0, 230)
+        pen.write("PLAYER 1: 0                                PLAYER 2: 0", align="center",
+                  font=("Lucida Sans", 24, "bold"))
+        pen.goto(0, 230)
+        pen.write("Click 'E' to go back to the main menu.", align="center",
+                  font=("Lucida Sans", 10, "normal"))
 
+
+        window.listen()
+        window.onkeypress(draw_menu, "e")
+
+        window.onkeypress(press_w, "w")
+        window.onkeyrelease(release_w, "w")
+
+        window.onkeypress(press_s, "s")
+        window.onkeyrelease(release_s, "s")
+
+        window.onkeypress(press_up, "Up")
+        window.onkeyrelease(release_up, "Up")
+
+        window.onkeypress(press_down, "Down")
+        window.onkeyrelease(release_down, "Down")
 
 # new paddle move function
 def pad_move():
@@ -193,6 +290,8 @@ while True:
             ding.play()
             pen.write("PLAYER 1: {}                                PLAYER 2: {}".format(scr_1, scr_2), align="center",
                       font=("Lucida Sans", 24, "bold"))
+            pen.write("Click 'E' to go back to the main menu.", align="center",
+                      font=("Lucida Sans", 10, "normal"))
 
         if ball.xcor() < -435:
             ball.goto(0, 0)
@@ -202,6 +301,8 @@ while True:
             ding.play()
             pen.write("PLAYER 1: {}                                PLAYER 2: {}".format(scr_1, scr_2), align="center",
                       font=("Lucida Sans", 24, "bold"))
+            pen.write("Click 'E' to go back to the main menu.", align="center",
+                      font=("Lucida Sans", 10, "normal"))
 
         if not pad_1.ycor() < 245:
             pad_1.sety(245)
@@ -228,5 +329,74 @@ while True:
         if (ball.xcor() < -390 and ball.xcor() > -400) and (ball.ycor() < pad_1.ycor()+ 65 and ball.ycor() > pad_1.ycor() - 65):
             ball.setx(-390)
             ball.dx *=-1
+            sound.play()
+
+    if game_state == "game2":
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+        # check da border
+        if ball.ycor() > 282:
+            ball.sety(282)
+            ball.dy *= -1.2
+            sound.play()
+
+        if ball.ycor() < -282:
+            ball.sety(-282)
+            ball.dy *= -1.2
+            sound.play()
+
+        if ball.xcor() > 435:
+            ball.goto(0, 0)
+            ball.dy = 1.2
+            ball.dx = 1.2
+
+            ball.dx *= -1
+            scr_1 += 1
+            pen.clear()
+            ding.play()
+            pen.write("PLAYER 1: {}                                PLAYER 2: {}".format(scr_1, scr_2), align="center",
+                      font=("Lucida Sans", 24, "bold"))
+            pen.write("Click 'E' to go back to the main menu.", align="center",
+                      font=("Lucida Sans", 10, "normal"))
+
+        if ball.xcor() < -435:
+            ball.goto(0, 0)
+            ball.dx = 1.2
+            ball.dy = 1.2
+
+            ball.dx *= -1
+            scr_2 += 1
+            pen.clear()
+            ding.play()
+            pen.write("PLAYER 1: {}                                PLAYER 2: {}".format(scr_1, scr_2), align="center",
+                      font=("Lucida Sans", 24, "bold"))
+            pen.write("Click 'E' to go back to the main menu.", align="center",
+                      font=("Lucida Sans", 10, "normal"))
+
+        if not pad_1.ycor() < 245:
+            pad_1.sety(245)
+
+
+        if not pad_1.ycor() > -245:
+            pad_1.sety(-245)
+
+        if not pad_2.ycor() < 245:
+            pad_2.sety(245)
+
+        if not pad_2.ycor() > -245:
+            pad_2.sety(-245)
+
+        # paddle ball collisions
+        if (ball.xcor() > 390 and ball.xcor() < 400) and (
+                ball.ycor() < pad_2.ycor() + 65 and ball.ycor() > pad_2.ycor() - 65):
+            ball.setx(390)
+            ball.dx *= -1.2
+            sound.play()
+
+        if (ball.xcor() < -390 and ball.xcor() > -400) and (
+                ball.ycor() < pad_1.ycor() + 65 and ball.ycor() > pad_1.ycor() - 65):
+            ball.setx(-390)
+            ball.dx *= -1.2
             sound.play()
 
